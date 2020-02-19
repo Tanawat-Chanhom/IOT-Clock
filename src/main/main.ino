a1a711a8d1c1a2c487e3833e07c938b1c5f18f74
Tanwat Chanhom <61070078@kmitl.ac.th>
2020-02-20 03:17:36 +0700

    Update main

--- src/main/main.ino
+++ src/main/main.ino
@@ -4,35 +4,61 @@
 #define IN2   D2
 #define IN3   D3
 #define IN4   D4
+
 #define IN5   D5
 #define IN7   D7
+
+int counter = 0;
+int val;
+int initStatus;
 
 const int stepsPerRevolution = 2048; // change this to fit the number of steps per revolution
 // initialize the stepper library
 Stepper myStepper(stepsPerRevolution, IN4, IN2, IN3, IN1);
 
 void setup() {
+  Serial.begin(115200);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
   pinMode(IN7, OUTPUT);
   pinMode(IN5, INPUT);
-  myStepper.setSpeed(13);
+  myStepper.setSpeed(10);
+  initStatus = _init();
 }
 
 void loop() {
-  int val = digitalRead(IN5);
-  
-  if(val == HIGH) //when magnetic field is detected, turn led on
-  {
-    digitalWrite(IN7, HIGH);
-    myStepper.step(10);
+  if (initStatus == 1) {
+    
+      if (counter >= 60) {
+        myStepper.step(-36);
+        if (counter == 120) {
+          counter = 0;
+        }
+      } else {
+        myStepper.step(36);
+      }
+
+//      myStepper.step(36);
+      counter++;
+      Serial.print(counter);
+      val = digitalRead(IN5);
+      Serial.println(val);
+      delay(10);
   }
-  else
-  {
-    digitalWrite(IN7, LOW);
-    myStepper.step(-10);
+}
+
+
+int _init() {
+  while(true) {
+    val = digitalRead(IN5);
+    Serial.println(val);
+    myStepper.step(-36);
+    if (val == LOW) {
+      counter = 0;
+      return 1;
+    }
+    delay(10);
   }
-  val = LOW;
 }
