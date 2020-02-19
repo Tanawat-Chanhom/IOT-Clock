e872c157ea17c52e31281b529115d3c8846adc46
Tanwat Chanhom <61070078@kmitl.ac.th>
2020-02-19 23:14:03 +0700

    Add something

--- src/exampleModuleController/KY-003/KY-003.ino
+++ src/exampleModuleController/KY-003/KY-003.ino
@@ -1,25 +1,26 @@
 #define IN3   D3   // IN1 is connected to NodeMCU pin D1 (GPIO5)
 #define IN5   D5   // IN1 is connected to NodeMCU pin D1 (GPIO5)
+#define IN7   D7
 int val; //numeric variable
 
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT); //set LED pin as output
-  pinMode(IN5, OUTPUT);
-  pinMode(IN3, INPUT); //set sensor pin as input
+  pinMode(IN5, INPUT);
+  pinMode(IN7, OUTPUT);
 }
 
 void loop()
 {
-  val = digitalRead(IN3); //Read the sensor
-  if(val == LOW) //when magnetic field is detected, turn led on
+  val = digitalRead(IN5); //Read the sensor
+  if(val == HIGH) //when magnetic field is detected, turn led on
   {
-    digitalWrite(LED_BUILTIN, LOW);
-    digitalWrite(IN5, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(IN7, HIGH);
   }
   else
   {
-    digitalWrite(LED_BUILTIN, HIGH);
-    digitalWrite(IN5, LOW);
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(IN7, LOW);
   }
 }
--- src/main/main.ino
+++ src/main/main.ino
@@ -1,9 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Stepper.h>
 
+#define IN1   D1
+#define IN2   D2
+#define IN3   D3
+#define IN4   D4
+#define IN5   D5
+#define IN7   D7
+
+const int stepsPerRevolution = 2048; // change this to fit the number of steps per revolution
+// initialize the stepper library
+Stepper myStepper(stepsPerRevolution, IN4, IN2, IN3, IN1);
+
+void setup() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(IN7, OUTPUT);
+  pinMode(IN5, INPUT);
+  myStepper.setSpeed(13);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int val = digitalRead(IN5);
+  
+  if(val == HIGH) //when magnetic field is detected, turn led on
+  {
+    digitalWrite(IN7, HIGH);
+    myStepper.step(10);
+  }
+  else
+  {
+    digitalWrite(IN7, LOW);
+    myStepper.step(-10);
+  }
+  val = LOW;
 }
