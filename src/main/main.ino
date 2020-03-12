bd322fcc968dfa35ecafcdffcbfd74b8bae01728
Tanwat Chanhom <61070078@kmitl.ac.th>
2020-03-13 02:35:33 +0700

    Update

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,22 +1,39 @@
 #include <Stepper.h>
 
+#define IN1   D1
+#define IN2   D2
+#define IN3   D3
+#define IN4   D4
+
+#define IN5   D5
+
+#define IN6   D6 
+#define IN7   D7
+
 int counter = 0;
 int val;
 int initStatus;
 int controller = 32;
+
+int start_hour = 11;
+int start_min = 0;
+
+int counter_stap = 1;
 
 const int stepsPerRevolution = 2048; // change this to fit the number of steps per revolution
 // initialize the stepper library
 Stepper myStepper(stepsPerRevolution, IN4, IN2, IN3, IN1);
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(D1, OUTPUT);
-  pinMode(D2, OUTPUT);
-  pinMode(D3, OUTPUT);
-  pinMode(D4, OUTPUT);
-  pinMode(D6, INPUT);
-  pinMode(D7, INPUT);
+  Serial.begin(9600);
+  
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  
+  pinMode(IN6, INPUT);
+  pinMode(IN7, INPUT);
   myStepper.setSpeed(12);
 //  initStatus = _init();
 }
@@ -29,13 +46,10 @@ void loop() {
     myStepper.step(34);
   } else if (val2 == HIGH) {
     delay(500);
-    int timeDeff = timeCal(12, 30);
+    int timeDeff = timeCal(start_hour, start_min);
     stapToTime(timeDeff);
-    Serial.println(timeDeff);
   }
-  delay(100);
 }
-
 
 int timeCal(int hour, int min) {
   int hourDeffToMin = (hour - 12) * 60;
@@ -44,11 +58,38 @@ int timeCal(int hour, int min) {
 }
 
 void stapToTime(int min) {
-  for(int i=0; i < min; i++){
-    delay(200);
-    myStepper.step(39);
-    Serial.println(i);
+  
+  if (min < 0) {
+    min = abs(min);
+    for(int i=0; i < min; i++){
+      delay(10);
+      myStepper.step(-39);
+      if (counter_stap == 60) {
+        Serial.println("In");
+        delay(500);
+        myStepper.step(-22);
+        delay(500);
+        counter_stap = 1;
+      } else {
+        counter_stap++;
+      }
+    }
+  } else {
+    for(int i=0; i < min; i++){
+      delay(10);
+      myStepper.step(39);
+      if (counter_stap == 60) {
+        Serial.println("In");
+        delay(500);
+        myStepper.step(22);
+        delay(500);
+        counter_stap = 1;
+      } else {
+        counter_stap++;
+      }
+    }  
   }
+  
 }
 
 int _init() {
